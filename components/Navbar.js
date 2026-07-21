e68aeb7f3d7f9ff611f70ab70ef2ce4f0a004d2e
padankcool <ngoerahboyz@gmail.com>
2026-07-21 11:01:46 +0800

    logo edit fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -120,7 +120,7 @@ export default function Navbar() {
               alt="Ubud Haven Logo" 
               width={300} 
               height={60} 
-              className="h-14 md:h-16 w-auto object-contain filter brightness-0 invert" 
+              className="h-15 md:h-17 w-auto object-contain filter brightness-0 invert" 
               priority
             />
           </Link>
